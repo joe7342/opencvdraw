9c3dae107674fbafadfced303fbbda19e296ff23
classjojo <jojo@ubuntu24.iiiedu.local>
2020-07-17 13:31:25 +0800

    0717 edit

--- vpen.py
+++ vpen.py
@@ -12,23 +12,21 @@ if load_from_disk:
 # 畫筆初始顏色
 pen_color = (255,0,0)
 
+thickness = 5
+
 cap = cv2.VideoCapture(0)
 
-# Load these 2 images and resize them to the same size.
 # 左上角切換功能圖片 resize
 pen_img = cv2.resize(cv2.imread('./img/pen.png',1), (60, 60))
 eraser_img = cv2.resize(cv2.imread('./img/eraser.png',1), (60, 60))
 
 kernel = np.ones((5,5),np.uint8)
 
-# Making window size adjustable
-cv2.namedWindow('image', cv2.WINDOW_AUTOSIZE)
+cv2.namedWindow('image', cv2.WINDOW_NORMAL)
 
-# This is the canvas on which we will draw upon
 # canvas 為畫布
 canvas = None
 
-# Create a background subtractor Object
 # 使用Background Subtractor MOG 2背景分離指令(不偵測陰影)
 backgroundobject = cv2.createBackgroundSubtractorMOG2(detectShadows = False)
 
@@ -36,24 +34,24 @@ backgroundobject = cv2.createBackgroundSubtractorMOG2(detectShadows = False)
 # 設定背景干擾的門檻值
 background_threshold = 600
 
-# A variable which tells you if you're using a pen or an eraser.
+# 告訴你正在使用比或橡皮擦的變數
 switch = 'Pen'
 
-# With this variable we will monitor the time between previous switch.
 # 此變數讓我們可以監控上一次切換所間隔的時間
 last_switch = time.time()
 
-# Initilize x1,y1 points
+# 初始化x1,y1座標
 x1,y1=0,0
 
 # Threshold for noise
 # 干擾閾值
 noiseth = 800
 
-# Threshold for wiper, the size of the contour must be bigger than this for # us to clear the canvas
+# Threshold for wiper, the size of the contour must be bigger than this for us to clear the canvas
+# 清除畫布的閾值,輪廓大小如超過此值就會清除畫布
 wiper_thresh = 40000
 
-# A variable which tells when to clear canvas
+# 告訴我們何時清除畫布的變數
 clear = False
 
 while(1):
@@ -63,24 +61,17 @@ while(1):
     
     cv2.putText(frame,'<- touch',(65,30),cv2.FONT_HERSHEY_DUPLEX, 0.6, (200,100,255), 1, cv2.LINE_AA)
     
-    # Initilize the canvas as a black image
     # 將畫布初始化為全黑圖片
     if canvas is None:
         canvas = np.zeros_like(frame)
         
-    # Take the top left of the frame and apply the background subtractor there
     # 取框架的左上角並應用背景分離
     top_left = frame[0: 50, 0: 50]
     fgmask = backgroundobject.apply(top_left)
     
-    
     # Note the number of pixels that are white, this is the level of disruption.
-    
     switch_thresh = np.sum(fgmask==255)
         
-    # If the disruption is greater than background threshold and there has 
-    # been some time after the previous switch then you can change the 
-    # object type.
     # 如果干擾大於背景閾值，且離上一次切換之後已有段時間，則可切換功能
     if switch_thresh>background_threshold and (time.time()-last_switch) > 1:
 
@@ -93,17 +84,15 @@ while(1):
             switch = 'Pen'
     
             
-    # Convert BGR to HSV
+    # BGR轉HSV
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     
-    # If you're reading from memory then load the upper and lower ranges 
-    # from there
     # 定義顏色範圍
     if load_from_disk:
-#             lower_range = blue[0]
-#             upper_range = blue[1]
-#             lower_range = red[0]
-#             upper_range = red[1]
+#         lower_range = blue[0]
+#         upper_range = blue[1]
+#         lower_range = red[0]
+#         upper_range = red[1]
         lower_range = yellow[0]
         upper_range = yellow[1]
         
@@ -112,31 +101,25 @@ while(1):
     # cv2.inRange()函數則是只顯示遮罩範圍內的顏色
     mask = cv2.inRange(hsv, lower_range, upper_range)
     
-    # Perform morphological operations to get rid of the noise
     # 執行形態學運算以消除雜訊
     mask = cv2.erode(mask,kernel,iterations = 1)
     mask = cv2.dilate(mask,kernel,iterations = 2)
     
-    # Find Contours
     # 找到物件邊緣
     contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, 
     cv2.CHAIN_APPROX_SIMPLE)
     
-    # Make sure there is a contour present and also it size is bigger than 
-    # noise threshold.
+    # 確保輪廓存在，並且輪廓的大小大於干擾閾值。
     if contours and cv2.contourArea(max(contours, key = cv2.contourArea)) > noiseth:
                 
         c = max(contours, key = cv2.contourArea)    
         x2,y2,w,h = cv2.boundingRect(c)
         
-        # Get the area of the contour
         # 取得輪廓範圍
         area = cv2.contourArea(c)
         ((x, y), radius) = cv2.minEnclosingCircle(c)
-        cv2.circle(frame, (int(x), int(y)), int(radius), (0, 165, 255), 2)
-        
-        # If there were no previous points then save the detected x2,y2 
-        # coordinates as x1,y1. 
+        cv2.circle(frame, (int(x), int(y)), int(radius), (120, 200, 255), 2)
+
         # 如果沒有先前的座標點，則將偵測到的x2,y2坐標另存為x1,y1。
         if x1 == 0 and y1 == 0:
             x1,y1= x2,y2
@@ -144,9 +127,8 @@ while(1):
         else:
 
             if switch == 'Pen':
-                # Draw the line on the canvas
-                # 畫筆顏色粗細
-                    canvas = cv2.line(canvas, (x1,y1), (x2,y2), pen_color, 5)
+                # 在畫布上畫線
+                canvas = cv2.line(canvas, (x1,y1), (x2,y2), pen_color, thickness)
                 
             else:
                 # 橡皮擦大小
@@ -161,16 +143,16 @@ while(1):
         # clear variable to True
         
         if area > wiper_thresh:
-            cv2.putText(canvas,'Reset Screen!',(95,240), 
+            cv2.putText(canvas,'Reset Screen!',(90,250), 
             cv2.FONT_HERSHEY_SIMPLEX, 2, (50,160,255), 3, cv2.LINE_AA)
-            clear = True
+            clear = True 
 
     else:
         # If there were no contours detected then make x1,y1 = 0
         x1,y1 = 0,0
     
    
-    # Now this piece of code is just for smooth drawing. (Optional)
+    # 下面五行是為了使繪圖更加平順
     _ , mask = cv2.threshold(cv2.cvtColor (canvas, cv2.COLOR_BGR2GRAY), 20, 
     255, cv2.THRESH_BINARY)
     foreground = cv2.bitwise_and(canvas, canvas, mask = mask)
@@ -178,7 +160,6 @@ while(1):
     mask = cv2.bitwise_not(mask))
     frame = cv2.add(foreground,background)
 
-    # Switch the images depending upon what we're using, pen or eraser.
     # 依照現在使用的功能切換左上角圖示
     if switch != 'Pen':
         cv2.circle(frame, (x1, y1), 30, (255,255,255), -1)
@@ -197,8 +178,7 @@ while(1):
     # 按s鍵將所繪線條存成.png檔
     elif k == ord('s'):
         cv2.imwrite("./img/img"+ strftime("%m%d_%H%M%S")+ ".png", canvas, [int(cv2.IMWRITE_PNG_COMPRESSION), 5])
-
-    # 顏色切換
+        
     elif k == ord('x'):
         pen_color = (0,0,255) # 紅色
         
@@ -212,8 +192,9 @@ while(1):
         pen_color = (255,0,0) # 藍色
     
     elif k == ord('z'):
-        pen_color =  0
-    
+        pen_color = 0,0 
+        thickness = 1
+
     # Clear the canvas after 1 second, if the clear variable is true
     if clear == True: 
         time.sleep(1)
